fdedf0b080d012564780df909a6fd6b3751bd748
HaKePlan <severin.clauss@gmail.com>
2022-05-29 21:53:45 +0200

    fix view

--- auftrag_a_abrfagen.sql
+++ auftrag_a_abrfagen.sql
@@ -1,14 +1,16 @@
 -- WDDA – Semesterarbeit: Teil a
 -- Frage 1:
+-- A)
 CREATE VIEW "view_California_real_estate" AS
-select C3.country_name as country,
+select Sale_Announcement.export_number as id,
+       C3.country_name as country,
        Sale_Announcement.date_posted as datePostedString,
        Sale_Announcement.is_bank_own as is_bankOwned,
        Sale_Announcement.is_for_auction as is_forAuction,
        S.status_name as event,
        Sale_Announcement.price as price,
        Sale_Announcement.price_per_square as pricePerSquareFoot,
-       C.city_name as city,
+       C2.city_name as city,
        S3.state_name as state,
        H.year_build as yearBuilt,
        S2.name as streetAddress,
@@ -33,7 +35,7 @@ select C3.country_name as country,
        Sale_Announcement.is_new_construction as isNewConstruction,
        Sale_Announcement.has_pets_allowed as hasPetsAllowed,
        HT.home_type_name as homeType,
-       C2.county_name as county
+       C.county_name as county
     from Sale_Announcement
     inner join Status S on S.id = Sale_Announcement.status_id
     inner join Home_Type HT on HT.id = Sale_Announcement.home_type_id
@@ -41,34 +43,27 @@ select C3.country_name as country,
     inner join Building_Area BA on BA.id = H.building_area_id
     inner join Unit U on BA.unit_id = U.id
     inner join Street S2 on S2.id = H.street_id
-    inner join City C on C.id = H.city_id
-    inner join County C2 on C2.id = C.county_id
+    inner join County C on C.id = H.county_id
+    inner join City C2 on C2.id = C.city_id
     inner join State S3 on S3.id = C2.state_id
     inner join Country C3 on C3.id = S3.country_id;
 
+drop view view_California_real_estate;
 
+-- B)
+SELECT COUNT(*) AS anzahl FROM "view_California_real_estate";
 
-SELECT COUNT(*) AS anzahl FROM view_RealEstate_California;
-
-
-
-SELECT DISTINCT *
-
-FROM "view_California_real-estate" WHERE city = "Bell Gardens";
+-- C)
+SELECT DISTINCT * FROM "view_California_real_estate" WHERE city = 'Bell Gardens';
 
 -- Frage 2:
-
-SELECT COUNT (yearBuilt) AS anzahl_immobilien2020
-
-FROM RealEstate_California
-
-WHERE yearBuilt != "N/A" AND yearBuilt > 2020
-
+SELECT COUNT (year_build) AS anzahl_immobilien2020 FROM House WHERE House.year_build > 2020;
 
 
 -- Frage 3:
+select distinct county, city from import_data order by city;
 
-
+select city_name, county_name from City inner join County C on C.id = City.county_id order by city_name;
 
 
 
